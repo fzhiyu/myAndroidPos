b55c72800d750ac0d745c0d9fbc497fbfa8b941e
fzhiyu <2017015437@qq.com>
2021-11-23 20:33:18 +0800

    v1.1

--- app/src/main/java/com/cuse/myandroidpos/Search.java
+++ app/src/main/java/com/cuse/myandroidpos/Search.java
@@ -12,7 +12,8 @@ import java.util.Calendar;
 
 public class Search extends AppCompatActivity {
 
-    Button date;
+    Button start_date;
+    Button end_date;
     DatePickerDialog datePickerDialog;
 
     @Override
@@ -21,9 +22,10 @@ public class Search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         // initiate the date picker and a button
-        date = (Button) findViewById(R.id.from_data);
+        start_date = (Button) findViewById(R.id.start_date);
+        end_date = (Button) findViewById(R.id.end_date);
         // perform click event on edit text
-        date.setOnClickListener(new View.OnClickListener() {
+        start_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // calender class's instance and get current date , month and year from calender
@@ -39,7 +41,7 @@ public class Search extends AppCompatActivity {
                             public void onDateSet(DatePicker view, int year,
                                                   int monthOfYear, int dayOfMonth) {
                                 // set day of month , month and year value in the edit text
-                                date.setText(dayOfMonth + "/"
+                                start_date.setText(dayOfMonth + "/"
                                         + (monthOfYear + 1) + "/" + year);
 
                             }
@@ -47,6 +49,29 @@ public class Search extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+        end_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(Search.this,
+                        new DatePickerDialog.OnDateSetListener() {
 
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                end_date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
     }
 }

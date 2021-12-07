3936ddcc47009e4cfcad397fec69d7c213143c49
hejun <fengz990702@gmail.com>
2021-12-07 11:19:00 +0800

    12_07第二次提交_0测试的数据需要写在前面，不然会出现bug

--- app/src/main/java/com/cuse/myandroidpos/ItemListFragment.java
+++ app/src/main/java/com/cuse/myandroidpos/ItemListFragment.java
@@ -58,73 +58,7 @@ public class ItemListFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //设置显示总金钱和总订单
-        tvTotalMoney = view.findViewById(R.id.tv_home_TodayTotalMoney);
-        tvTotalMoney.setText(orderLastJson.getData().getTodayMoney() + "");
-        tvTotalOrder = view.findViewById(R.id.tv_home_TodayTotalOrder);
-        tvTotalOrder.setText(orderLastJson.getData().getTodayCount() + "");
-
-        //找到按钮实例,搜索，汇总，退单，设置
-        btnSearch = view.findViewById(R.id.btn_home_search);
-        btnAll = view.findViewById(R.id.btn_home_all);
-        btnRefund = view.findViewById(R.id.btn_home_refund);
-        btnSet = view.findViewById(R.id.btn_home_set);
-
-        //搜索按钮
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(getView()).navigate(R.id.show_search, null);
-            }
-        });
-
-        //汇总按钮
-        btnAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(getView()).navigate(R.id.show_count, null);
-            }
-        });
-
-        //退单按钮
-        btnRefund.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(getView()).navigate(R.id.show_back, null);
-            }
-        });
-
-        //设置按钮
-        btnSet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(getView()).navigate(R.id.show_setting, null);
-            }
-        });
-//recycleView,适配器单独写在了HomeAdapter
-        RecyclerView recyclerView = binding.itemList;
-        //设置竖直
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(linearLayoutManager);
-        //将数据填入homeAdapt
-        HomeAdapter homeAdapter = new HomeAdapter(orderLastJson.getData().getOilOrderList(),getActivity());
-        recyclerView.setAdapter(homeAdapter);
-        //设置点击事件，点击事件的接口定义哎HomeAdapter
-        homeAdapter.setHomeRecyclerItemClickListener(new HomeAdapter.OnHomeRecyclerItemClickListener() {
-            @Override
-            public void OnHomeRecyclerItemClick(int position) {
-                Bundle bundle = new Bundle();
-
-                //用bundle来传输对象（传输的是OrderLastJson包里面的OilOrderList对象），OilOrderList类需要implements Serializable
-                // 就可以把bundle.putString换成，bundle.putSerializable
-                //详情界面可以用oilOrder = (OilOrderList) bundle.getSerializable("LastOilOrder");来得到对象
-                bundle.putSerializable("LastOilOrder",orderLastJson.getData().getOilOrderList().get(position));
-
-                Navigation.findNavController(getView()).navigate(R.id.show_item_detail,bundle);
-            }
-        });
-
-        //测试的数据，不用管
+        //测试的数据，不用管，但是需要写在前面，不然会出现bug
         orderLastJson = new OrderLastJson();
         sJson = "{\n" +
                 "  \"code\": 0,\n" +
@@ -249,8 +183,71 @@ public class ItemListFragment extends Fragment {
 
         orderLastJson = new Gson().fromJson(sJson,OrderLastJson.class);
 
+        //设置显示总金钱和总订单
+        tvTotalMoney = view.findViewById(R.id.tv_home_TodayTotalMoney);
+        tvTotalMoney.setText(orderLastJson.getData().getTodayMoney() + "");
+        tvTotalOrder = view.findViewById(R.id.tv_home_TodayTotalOrder);
+        tvTotalOrder.setText(orderLastJson.getData().getTodayCount() + "");
 
+        //找到按钮实例,搜索，汇总，退单，设置
+        btnSearch = view.findViewById(R.id.btn_home_search);
+        btnAll = view.findViewById(R.id.btn_home_all);
+        btnRefund = view.findViewById(R.id.btn_home_refund);
+        btnSet = view.findViewById(R.id.btn_home_set);
 
+        //搜索按钮
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.show_search, null);
+            }
+        });
+
+        //汇总按钮
+        btnAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.show_count, null);
+            }
+        });
+
+        //退单按钮
+        btnRefund.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.show_back, null);
+            }
+        });
+
+        //设置按钮
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getView()).navigate(R.id.show_setting, null);
+            }
+        });
+//recycleView,适配器单独写在了HomeAdapter
+        RecyclerView recyclerView = binding.itemList;
+        //设置竖直
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //将数据填入homeAdapt
+        HomeAdapter homeAdapter = new HomeAdapter(orderLastJson.getData().getOilOrderList(),getActivity());
+        recyclerView.setAdapter(homeAdapter);
+        //设置点击事件，点击事件的接口定义哎HomeAdapter
+        homeAdapter.setHomeRecyclerItemClickListener(new HomeAdapter.OnHomeRecyclerItemClickListener() {
+            @Override
+            public void OnHomeRecyclerItemClick(int position) {
+                Bundle bundle = new Bundle();
+
+                //用bundle来传输对象（传输的是OrderLastJson包里面的OilOrderList对象），OilOrderList类需要implements Serializable
+                // 就可以把bundle.putString换成，bundle.putSerializable
+                //详情界面可以用oilOrder = (OilOrderList) bundle.getSerializable("LastOilOrder");来得到对象
+                bundle.putSerializable("LastOilOrder",orderLastJson.getData().getOilOrderList().get(position));
+
+                Navigation.findNavController(getView()).navigate(R.id.show_item_detail,bundle);
+            }
+        });
 
 
     }

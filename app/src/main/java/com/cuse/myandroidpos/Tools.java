b48bde237535ded1ea3a621259fa35dea09b4928
hejun <fengz990702@gmail.com>
2022-01-05 22:10:25 +0800

    2.7改正签名错误

--- app/src/main/java/com/cuse/myandroidpos/Tools.java
+++ app/src/main/java/com/cuse/myandroidpos/Tools.java
@@ -1,5 +1,8 @@
 package com.cuse.myandroidpos;
 
+import android.content.Context;
+import android.widget.Toast;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -30,5 +33,39 @@ public class Tools {
         s = df.format(new Date(stamp));
 
         return s;
+    }
+
+    //显示code不为0时候的错误
+    public static void codeError(Context context, int code) {
+        switch (code) {
+            case 1: {
+                Toast.makeText(context, "时间戳错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 2: {
+                Toast.makeText(context, "签名错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 3: {
+                Toast.makeText(context, "参数错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 4: {
+                Toast.makeText(context, "油站不存在", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 5: {
+                Toast.makeText(context, "密码错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 6: {
+                Toast.makeText(context, "token错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+            case 999: {
+                Toast.makeText(context, "未知错误", Toast.LENGTH_SHORT).show();
+            }
+            break;
+        }
     }
 }
